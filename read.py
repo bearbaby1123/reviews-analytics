a774f37eb7dc7b8445478b9bbc31dddd5cc40d59
Jacky Hsiung <forlol1123@gmail.com>
2019-02-23 13:03:55 +0800

    add average length

--- read.py
+++ read.py
@@ -1,10 +1,13 @@
 data = []
+total_length = 0
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
+		total_length += len(line) 
 		data.append(line.strip())
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
-
-print(data[0])
+		#count += 1
+		#if count % 1000 == 0:
+			#print(len(data))
+average_length = total_length / len(data)
+print('The file is completely loaded, there is', len(data), 'pieces of comments')
+print('The average length of the comment is', average_length)
